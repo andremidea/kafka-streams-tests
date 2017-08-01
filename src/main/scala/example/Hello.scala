ba046089aca49c2a7c22cba587bd5d526d33eeb1
André Midea <midea.andre@gmail.com>
2017-08-01 18:14:24 -0300

    foo2

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -7,10 +7,10 @@ import java.util.concurrent.TimeUnit
 import org.apache.kafka.common.serialization._
 import org.apache.kafka.streams._
 import org.apache.kafka.streams.kstream._
-
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.serialization.Serdes.StringSerde
-
+import org.apache.kafka.streams.processor.{Processor, ProcessorContext, ProcessorSupplier, TopologyBuilder}
+import org.apache.kafka.streams.state.{KeyValueStore, Stores}
 import scala.collection.mutable
 
 class BatchInitializer extends Initializer[Batch] {
@@ -27,9 +27,48 @@ class Batch {
     this
   }
 }
-object Hello extends Greeting with App {
-  Foo.euclidesLikesHillary
 
+class Foo extends Processor[String, String] {
+
+  var processor: ProcessorContext = null
+  var state: KeyValueStore[String, String] = null
+  val k = "chupa_conrado2"
+
+  override def init(context: ProcessorContext): Unit = {
+    processor = context
+    context.schedule(10000)
+    state = context.getStateStore("FOO").asInstanceOf[KeyValueStore[String, String]]
+  }
+
+  override def process(key: String, value: String): Unit = {
+    val s: String = state.get(k)
+    println(s)
+
+    if(s != null) {
+      println("not null")
+      state.put(k, s + " | " + value)
+    } else {
+      state.put(k, ">>>>>> " + value)
+    }
+  }
+
+  override def punctuate(timestamp: Long): Unit = {
+    val s = state.get(k)
+
+    if (s != null) {
+      print(s.size)
+      if(s.size > 1000) {
+        state.delete(k)
+        processor.forward(k, s)
+        processor.commit()
+      }
+    }
+  }
+
+  override def close(): Unit = {}
+}
+
+object Hello extends Greeting with App {
   val name = "app03"
   val config: Properties = {
     val p = new Properties()
@@ -40,23 +79,41 @@ object Hello extends Greeting with App {
     p
   }
 
-  val i1: Initializer[String]                                     = () => s"XXX>>>>>"
-  val aggregator: Aggregator[String, String, String]              = (k, v, va) => va.concat(" | ").concat(v)
-  val keyMapper: KeyValueMapper[Windowed[String], String, String] = (k, v) => k.key() // we don't want to change the key
-  val valueMapper: ValueMapper[String, String]                    = (v) => v
+  val proc: ProcessorSupplier[String, String] = () => new Foo()
 
-  val serdeString: Serde[String] = Serdes.String()
+  val store = Stores.create("FOO")
+    .withKeys(Serdes.String())
+    .withValues(Serdes.String())
+    .persistent()
+    .build()
 
-  val builder: KStreamBuilder               = new KStreamBuilder()
-  val streamEvents: KStream[String, String] = builder.stream("stream-events")
-  val aggregate: KTable[Windowed[String], String] = streamEvents
-    .groupByKey()
-    .aggregate(i1, aggregator, TimeWindows.of(30000), serdeString, "foo")
+  val builder2: TopologyBuilder = new TopologyBuilder()
+  builder2
+    .addSource("SOURCE", "stream-events")
+    .addProcessor("AGGREGATE", proc, "SOURCE")
+    .addSink("SINK", "stream-events-agg", "AGGREGATE")
+    .addStateStore(store, "AGGREGATE")
+    .connectProcessorAndStateStores("AGGREGATE", "FOO")
 
 
-  val stream: KStream[String, String] = aggregate.toStream(keyMapper)
 
-  stream.to(Serdes.String(), Serdes.String(), "stream-events-agg2")
+//  val i1: Initializer[String]                                     = () => s"XXX>>>>>"
+//  val aggregator: Aggregator[String, String, String]              = (k, v, va) => va.concat(" | ").concat(v)
+//  val keyMapper: KeyValueMapper[Windowed[String], String, String] = (k, v) => k.key() // we don't want to change the key
+//  val valueMapper: ValueMapper[String, String]                    = (v) => v
+//
+//  val serdeString: Serde[String] = Serdes.String()
+//
+//  val builder: KStreamBuilder               = new KStreamBuilder()
+//  val streamEvents: KStream[String, String] = builder.stream("stream-events")
+//  val aggregate: KTable[Windowed[String], String] = streamEvents
+//    .groupByKey()
+//    .aggregate(i1, aggregator, TimeWindows.of(30000), serdeString, "foo")
+//
+//
+//  val stream: KStream[String, String] = aggregate.toStream(keyMapper)
+//
+//  stream.to(Serdes.String(), Serdes.String(), "stream-events-agg2")
 
 //  val wordCounts: KTable[String, Long] = textLines
 //    .flatMapValues(textLine => textLine.toLowerCase.split("\\W+").toIterable.asJava)
@@ -66,7 +123,7 @@ object Hello extends Greeting with App {
 
 //  aggregate.to(serdeString, serdeString, "stream-events-agg")
 
-  val streams: KafkaStreams = new KafkaStreams(builder, config)
+  val streams: KafkaStreams = new KafkaStreams(builder2, config)
 
   streams.start()
 
@@ -87,13 +144,18 @@ object Foo {
     kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
     val producer = new KafkaProducer[String, String](kafkaProps)
-
-    Range(0, 10)
-      .partition(_ % 2 == 0) match {
-      case (a, b) =>
-        val y = (x: Int) => Instant.now().toEpochMilli + (x * 2000)
-        a.foreach(x => producer.send(new ProducerRecord("stream-events",x % 2 , y(x), "value", x.toString.concat("-").concat(y(x).toString))))
-        b.foreach(x => producer.send(new ProducerRecord("stream-events", x % 2, y(x), "value", x.toString.concat("-").concat(y(x).toString))))
+    def round[A](seq: Iterable[A], n: Int) = {
+      (0 until n).map(i => seq.drop(i).sliding(1, n).flatten)
+    }
+    round(Stream.from(1), 1000).foreach { x =>
+      x
+        .partition(_ % 2 == 0) match {
+        case (a, b) =>
+          val y = (x: Int) => Instant.now().toEpochMilli + (x * 2000)
+          a.foreach(x => producer.send(new ProducerRecord("stream-events", x % 2, y(x), "value", x.toString.concat("-").concat(y(x).toString))))
+          b.foreach(x => producer.send(new ProducerRecord("stream-events", x % 2, y(x), "value", x.toString.concat("-").concat(y(x).toString))))
+      }
+      Thread.sleep(5000)
     }
   }
 }
